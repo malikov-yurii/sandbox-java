628a98f68908afab452dc59d3943b7dee17c4d7f
Yurii Malikov <yurii.malikov@northrockllc.com>
2022-12-09 20:11:58 +0200

    T_2_1_ReturnKthToLast

---
+++ src/main/java/cci/ch_2_linked_lists/T_2_1_ReturnKthToLast.java
@@ -0,0 +1,56 @@
+package cci.ch_2_linked_lists;
+
+/* Remove dups from unordered linked list */
+public class T_2_1_ReturnKthToLast {
+
+    //    Runtime : O(n)
+    //    Memory  : O(1)
+    public static Node returnKthToLast(Node head, int k) {
+        Node p1 = head;
+        Node p2 = head;
+        for (int i = 0; i < k - 1; i++) {
+            if (p1.next == null) {
+                return null;
+            }
+            p1 = p1.next;
+        }
+        while (p1.next != null) {
+            p1 = p1.next;
+            p2 = p2.next;
+        }
+        return p2;
+    }
+
+
+    //    Runtime : O(n)
+    //    Memory  : O(n)
+    public static Node returnKthToLastRecursively(Node head, int k) {
+        NodeSearch nodeSearch = new NodeSearch(k);
+        searchKthToLast(head, nodeSearch);
+        return nodeSearch.result;
+    }
+
+    private static void searchKthToLast(Node node, NodeSearch nodeSearch) {
+        if (node == null) {
+            return;
+        }
+        nodeSearch.maxDepth++;
+        nodeSearch.currentDepth++;
+        searchKthToLast(node.next, nodeSearch);
+        if (nodeSearch.maxDepth - nodeSearch.currentDepth + 1 == nodeSearch.resultNodeDistanceToLast) {
+            nodeSearch.result = node;
+        }
+        nodeSearch.currentDepth--;
+    }
+
+    static class NodeSearch {
+        int maxDepth;
+        int currentDepth;
+        int resultNodeDistanceToLast;
+        Node result;
+        public NodeSearch(int resultNodeDistanceToLast) {
+            this.resultNodeDistanceToLast = resultNodeDistanceToLast;
+        }
+    }
+
+}
