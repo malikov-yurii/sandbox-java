fc840d4fc52495f120e064776a5f7aca2c5815d7
Yurii Malikov <yurii.malikov@northrockllc.com>
2022-12-17 09:43:33 +0200

    T_2_6_PalindromeLinkedList recursive

--- src/main/java/cci/ch_2_linked_lists/T_2_6_PalindromeLinkedList.java
+++ src/main/java/cci/ch_2_linked_lists/T_2_6_PalindromeLinkedList.java
@@ -2,6 +2,56 @@ package cci.ch_2_linked_lists;
 
 public class T_2_6_PalindromeLinkedList {
 
+    public static boolean checkRecursive(Node<Character> node) {
+        PalindromeVerification verification = new PalindromeVerification();
+        verification.left = node;
+        verification.right = node;
+        verification.len = countLetters(node);
+        verification.leaves = verification.len / 2;
+        checkRecursive(verification);
+        return verification.isPalindrome;
+    }
+
+    private static void checkRecursive(PalindromeVerification verification) {
+        skipSpaces(verification);
+        Node<Character> left = verification.left;
+        verification.leaves--;
+        if (verification.leaves == 0) {
+            if (verification.len % 2 == 1) {
+                verification.right = verification.right.next;
+                skipSpaces(verification);
+            }
+            verification.right = verification.right.next;
+            skipSpaces(verification);
+        }
+        if (verification.leaves > 0) {
+            verification.left = verification.left.next;
+            verification.right = verification.right.next;
+            checkRecursive(verification);
+        }
+        verification.isPalindrome &= left.data == verification.right.data;
+        verification.right = verification.right.next;
+        skipSpaces(verification);
+    }
+
+    private static void skipSpaces(PalindromeVerification verification) {
+        while (verification.left != null && verification.left.data == ' ') {
+            verification.left = verification.left.next;
+        }
+        while (verification.right != null && verification.right.data == ' ') {
+            verification.right = verification.right.next;
+        }
+    }
+
+    static class PalindromeVerification {
+        Node<Character> left;
+        Node<Character> right;
+        int leaves;
+        int len;
+        boolean isPalindrome = true;
+    }
+
+
     public static boolean check(Node<Character> node) {
         int len = countLetters(node);
         Node<Character> reverse = null;
@@ -22,10 +72,6 @@ public class T_2_6_PalindromeLinkedList {
         while (reverse != null) {
             if (node.data == ' ') {
                 node = node.next;
-                continue;
-            }
-            if (reverse.data == ' ') {
-                reverse = reverse.next;
                 continue;
             }
             if (reverse.data != node.data) {
--- src/test/java/cci/ch_2_linked_lists/T_2_6_PalindromeLinkedListTest.java
+++ src/test/java/cci/ch_2_linked_lists/T_2_6_PalindromeLinkedListTest.java
@@ -7,44 +7,103 @@ import static org.assertj.core.api.Assertions.assertThat;
 class T_2_6_PalindromeLinkedListTest {
 
     @Test
+    void checkRecursive1() {
+        Node<Character> node = new Node<>('1', new Node<>('2', new Node<>('3', new Node<>('2', new Node<>('1', null)))));
+        assertThat(T_2_6_PalindromeLinkedList.checkRecursive(node))
+                .isTrue();
+    }
+
+    @Test
+    void checkRecursive2() {
+        Node<Character> node = new Node<>('1', new Node<>('2', new Node<>('2', new Node<>('1', null))));
+        assertThat(T_2_6_PalindromeLinkedList.checkRecursive(node))
+                .isTrue();
+    }
+
+    @Test
+    void checkRecursive3() {
+        Node<Character> node = new Node<>(' ', new Node<>('d', new Node<>(' ', new Node<>('c', new Node<>(' ', new Node<>(' ',
+                new Node<>('c', new Node<>('d', null))))))));
+        assertThat(T_2_6_PalindromeLinkedList.checkRecursive(node))
+                .isTrue();
+    }
+
+    @Test
+    void checkRecursive4() {
+        assertThat(T_2_6_PalindromeLinkedList.checkRecursive(new Node<>('d', new Node<>('c', new Node<>('b',
+                new Node<>('c', new Node<>('d', new Node<>(' ', null))))))))
+                .isTrue();
+    }
+
+    @Test
+    void checkRecursive5() {
+        assertThat(T_2_6_PalindromeLinkedList.checkRecursive(new Node<>('d', new Node<>('c', new Node<>('b', new Node<>('a', new Node<>(' ',
+                new Node<>('b', new Node<>('c', new Node<>('d', null))))))))))
+                .isTrue();
+    }
+
+    @Test
+    void checkRecursive6() {
+        assertThat(T_2_6_PalindromeLinkedList.checkRecursive(new Node<>('a', new Node<>('c', new Node<>(
+                'c', new Node<>('d', null))))))
+                .isFalse();
+    }
+
+    @Test
+    void checkRecursive7() {
+        assertThat(T_2_6_PalindromeLinkedList.checkRecursive(new Node<>('d', new Node<>('a', new Node<>('b',
+                new Node<>('c', new Node<>('d', null)))))))
+                .isFalse();
+    }
+
+    @Test
+    void checkRecursive8() {
+        assertThat(T_2_6_PalindromeLinkedList.checkRecursive(new Node<>('d', new Node<>('c', new Node<>('b',
+                new Node<>('a', new Node<>('b', new Node<>('a', new Node<>('d', null)))))))))
+                .isFalse();
+    }
+
+
+    @Test
     void check1() {
-        assertThat(T_2_6_PalindromeLinkedList.check(new Node<>(' ', new Node<>('4', new Node<>(' ', new Node<>('3',
-                                                    new Node<>(' ', new Node<>(' ', new Node<>('3', new Node<>('4', null))))))))))
+        Node<Character> node = new Node<>(' ', new Node<>('d', new Node<>(' ', new Node<>('c', new Node<>(' ', new Node<>(' ',
+                                                    new Node<>('c', new Node<>('d', null))))))));
+        assertThat(T_2_6_PalindromeLinkedList.check(node))
                 .isTrue();
     }
 
     @Test
     void check2() {
-        assertThat(T_2_6_PalindromeLinkedList.check(new Node<>('4', new Node<>('3', new Node<>('2',
-                                                    new Node<>('3', new Node<>('4', new Node<>(' ', null))))))))
+        assertThat(T_2_6_PalindromeLinkedList.check(new Node<>('d', new Node<>('c', new Node<>('b',
+                                                    new Node<>('c', new Node<>('d', new Node<>(' ', null))))))))
                 .isTrue();
     }
 
     @Test
     void check3() {
-        assertThat(T_2_6_PalindromeLinkedList.check(new Node<>('4', new Node<>('3', new Node<>('2', new Node<>('1', new Node<>(' ',
-                                                    new Node<>('2', new Node<>('3', new Node<>('4', null))))))))))
+        assertThat(T_2_6_PalindromeLinkedList.check(new Node<>('d', new Node<>('c', new Node<>('b', new Node<>('a', new Node<>(' ',
+                                                    new Node<>('b', new Node<>('c', new Node<>('d', null))))))))))
                 .isTrue();
     }
 
     @Test
     void check4() {
-        assertThat(T_2_6_PalindromeLinkedList.check(new Node<>('1', new Node<>('3', new Node<>(
-                                                               '3', new Node<>('4', null))))))
+        assertThat(T_2_6_PalindromeLinkedList.check(new Node<>('a', new Node<>('c', new Node<>(
+                                                               'c', new Node<>('d', null))))))
                 .isFalse();
     }
 
     @Test
     void check5() {
-        assertThat(T_2_6_PalindromeLinkedList.check(new Node<>('4', new Node<>('1', new Node<>('2',
-                                                    new Node<>('3', new Node<>('4', null)))))))
+        assertThat(T_2_6_PalindromeLinkedList.check(new Node<>('d', new Node<>('a', new Node<>('b',
+                                                    new Node<>('c', new Node<>('d', null)))))))
                 .isFalse();
     }
 
     @Test
     void check6() {
-        assertThat(T_2_6_PalindromeLinkedList.check(new Node<>('4', new Node<>('3', new Node<>('2',
-                               new Node<>('1', new Node<>('2', new Node<>('1', new Node<>('4', null)))))))))
+        assertThat(T_2_6_PalindromeLinkedList.check(new Node<>('d', new Node<>('c', new Node<>('b',
+                               new Node<>('a', new Node<>('b', new Node<>('a', new Node<>('d', null)))))))))
                 .isFalse();
     }
 }
