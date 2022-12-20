9dee3492472a702cb9b80042ded0be9248107114
Yurii Malikov <yurii.malikov@northrockllc.com>
2022-12-20 19:17:13 +0200

    T_3_5_SortStackUsingAdditionalStack

---
+++ src/main/java/cci/ch_2_stack_and_queue/T_3_5_SortStackUsingAdditionalStack.java
@@ -0,0 +1,30 @@
+package cci.ch_2_stack_and_queue;
+
+import java.util.Stack;
+
+public class T_3_5_SortStackUsingAdditionalStack {
+
+    public static <T extends Comparable<T>> void sort(Stack<T> stack) {
+        Stack<T> tmpStack = new Stack<>();
+        while (!stack.isEmpty()) {
+            T element = stack.pop();
+            if (tmpStack.isEmpty() || element.compareTo(tmpStack.peek()) >= 0) {
+                tmpStack.push(element);
+            } else {
+                int count = 0;
+                do {
+                    count++;
+                    stack.push(tmpStack.pop());
+                } while (!tmpStack.isEmpty() && element.compareTo(tmpStack.peek()) < 0);
+                tmpStack.push(element);
+                while (count-- != 0) {
+                    tmpStack.push(stack.pop());
+                }
+            }
+        }
+        while (!tmpStack.isEmpty()) {
+            stack.push(tmpStack.pop());
+        }
+    }
+
+}
---
+++ src/test/java/cci/ch_2_stack_and_queue/T_3_5_SortStackUsingAdditionalStackTest.java
@@ -0,0 +1,30 @@
+package cci.ch_2_stack_and_queue;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Stack;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class T_3_5_SortStackUsingAdditionalStackTest {
+
+    @Test
+    void sort() {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(1);
+        stack.push(4);
+        stack.push(3);
+        stack.push(22);
+        stack.push(55);
+        stack.push(44);
+
+        T_3_5_SortStackUsingAdditionalStack.sort(stack);
+
+        assertThat(stack.pop()).isEqualTo(1);
+        assertThat(stack.pop()).isEqualTo(3);
+        assertThat(stack.pop()).isEqualTo(4);
+        assertThat(stack.pop()).isEqualTo(22);
+        assertThat(stack.pop()).isEqualTo(44);
+        assertThat(stack.pop()).isEqualTo(55);
+    }
+}
