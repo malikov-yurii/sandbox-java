8eba5b0d15c0eac2d61005f15061972e158dc234
Yurii Malikov <yurii.malikov@northrockllc.com>
2022-12-20 19:47:37 +0200

    T_3_6_AnimalShelter_TODO

---
+++ src/main/java/cci/ch_2_stack_and_queue/T_3_6_AnimalShelter_TODO.java
@@ -0,0 +1,5 @@
+package cci.ch_2_stack_and_queue;
+
+// TODO: 20.12.2022 Not sure need to implement. It is simple : 2 queues and verify which is older using increased seq number
+public class T_3_6_AnimalShelter_TODO {
+}
